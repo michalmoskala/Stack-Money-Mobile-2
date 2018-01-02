ed2b256d74fc8506996337f9278ee33bc0ca4044
Kamil Golec <kumalgfilms@gmail.com>
2018-01-02 17:02:04 +0100

    accounts recycleView

---
+++ app/src/main/java/com/example/zbyszek/stackmoney2/model/account/Balance.kt
@@ -0,0 +1,17 @@
+package com.example.zbyszek.stackmoney2.model.account
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import android.arch.persistence.room.PrimaryKey
+import com.example.zbyszek.stackmoney2.model.Color
+import com.example.zbyszek.stackmoney2.model.User
+
+data class Balance(
+        @ColumnInfo(name = "balance")
+        var balance : Long
+) {
+    @ColumnInfo(name = "id")
+    @PrimaryKey(autoGenerate = true)
+    var id : Long = 0
+}
--- app/src/main/java/com/example/zbyszek/stackmoney2/sql/dao/AccountDAO.kt
+++ app/src/main/java/com/example/zbyszek/stackmoney2/sql/dao/AccountDAO.kt
@@ -2,6 +2,7 @@ package com.example.zbyszek.stackmoney2.sql.dao
 
 import android.arch.persistence.room.*
 import com.example.zbyszek.stackmoney2.model.account.AccountSQL
+import com.example.zbyszek.stackmoney2.model.account.Balance
 import com.example.zbyszek.stackmoney2.model.account.BindedAccountSQL
 
 @Dao
@@ -25,21 +26,20 @@ interface AccountDAO {
     @Delete()
     fun deleteAccountSQL(accountSQL : AccountSQL)
 
-//    @Query("SELECT " +
-//            "MAIN.id, " +
-//            "MAIN.name, " +
-//            "(SELECT TOTAL(CASE WHEN is_expense THEN -cost " +
-//            "ELSE cost END) " +
-//            "FROM operations " +
-//            "WHERE user_id IS :userId AND account_id IN (MAIN.id, " +
-//            "(SELECT DISTINCT id " +
-//            "FROM accounts " +
-//            "Where user_id IS :userId AND parent_account_id = MAIN.id)) " +
-//            "AND date != '' " +
-//            "AND date <= date('now')) / 100.0 AS Balance " +
-//            "FROM accounts MAIN " +
-//            "WHERE MAIN.user_id IS :userId AND MAIN.parent_account_id IS NULL " +
-//            "ORDER BY MAIN.name\n")
-//    fun getAllUserAccountsBalances(userId : Long) : List<Long>
+    @Query("SELECT " +
+            "MAIN.id AS id, " +
+            "(SELECT TOTAL(CASE WHEN is_expense THEN -cost " +
+            "ELSE cost END) AS balance" +
+            "FROM operations " +
+            "WHERE user_id IS :userId AND account_id IN (MAIN.id, " +
+            "(SELECT DISTINCT id " +
+            "FROM accounts " +
+            "Where user_id IS :userId AND parent_account_id = MAIN.id)) " +
+            "AND date != '' " +
+            "AND date <= date('now')) / 100.0 AS Balance " +
+            "FROM accounts MAIN " +
+            "WHERE MAIN.user_id IS :userId AND MAIN.parent_account_id IS NULL " +
+            "ORDER BY MAIN.name\n")
+    fun getAllUserAccountsBalances(userId : Long) : List<Balance>
 
 }
